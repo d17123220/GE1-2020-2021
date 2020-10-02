3cf62f300b4eeb40f7daccaa57c7a56a08219858
Kseniia Klimenko <d17123220@mytudublin.ie>
2020-10-02 21:41:14 +0100

    Part A of the lab

---
+++ Game Engines Examples/Assets/Generator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int ringnum = 9;
+    public int ringbaserad = 1;
+    public int elementSize = 1;
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int j = 1; j < ringnum + 1; j++)
+        {
+            int ringrad = ringbaserad * j;
+
+            float elementsInRing = Mathf.Floor(Mathf.PI * 2.0f * (float)ringrad / (float)elementSize); 
+            float ugol = Mathf.PI * 2.0f / (float) elementsInRing;
+            for (int i = 0; i < elementsInRing; i++)
+            {
+                GameObject sp = GameObject.Instantiate<GameObject>(prefab);
+                Vector3 pos = new Vector3(Mathf.Sin(ugol * i) * ringrad,Mathf.Cos(ugol * i) * ringrad, 0);
+                sp.transform.position = transform.TransformPoint(pos);
+
+                sp.GetComponent<Renderer>().material.color = 
+                    Color.HSVToRGB(1.0f / elementsInRing * i, 1, 1.0f - 0.5f * j / ringnum);
+                    //             color hue divided by whole ring,
+                    //                                        saturation = 100%,
+                    //                                           Intensity - 1.0f at the center and dimming to 0.5f at the edge
+                    
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game Engines Examples/Assets/rotate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float speed = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float dangle = speed * Time.deltaTime;
+
+        //get position and change angle/position based on distance to center
+        /*
+        Vector3 pos = transform.position;
+        
+        Vector3 newpos =  new Vector3(pos.x * Mathf.Cos(dangle) + pos.y * Mathf.Sin(dangle),  pos.y * Mathf.Cos(dangle) - pos.x * Mathf.Sin(dangle), 0.0f);
+        transform.position = newpos;
+        */
+
+        // rotate each prefab around it's Y (vertical) axis
+
+        Vector3 rotate = new Vector3(0,Random.Range(0.0f,1.1f),0);
+        transform.RotateAround(transform.position, rotate, dangle);
+
+    }
+}
